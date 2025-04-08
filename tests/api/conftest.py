050bdf15b555295c0a5ec45e1bad935ad9bd0d64
Tom Roberts <tom.roberts@ucl.ac.uk>
2025-04-08 16:36:53 +0100

    Bugfix: incorrect pathing to fastapi entrypoint

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,11 +1,19 @@
+import os
 import subprocess
 import time
 import pytest
 
 @pytest.fixture(scope="module")
 def start_api_app():
+    """
+    Starts the forwarding API on the locally host machine
+    - Note: does not start OCR tool APIs
+    """
+    if not os.path.isfile("src/api/app/main.py"):
+        raise Exception("Cannot find main.py to start API.")
+    
     proc = subprocess.Popen(
-        ["fastapi", "run", "main.py", "--host", "127.0.0.1", "--port", "8080"],
+        ["fastapi", "run", "src/api/app/main.py", "--host", "127.0.0.1", "--port", "8080"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
