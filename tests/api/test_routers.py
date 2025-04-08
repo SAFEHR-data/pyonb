551c9f5aa43c3133fb1ce3b0ff21e8f3a1bec07b
Tom Roberts <tom.roberts@ucl.ac.uk>
2025-04-08 18:01:23 +0100

    Adds marker inference unit test

--- tests/api/test_routers.py
+++ tests/api/test_routers.py
@@ -18,3 +18,14 @@ def test_router_sparrow(start_api_app_docker) -> None:
     response = requests.get("http://127.0.0.1:8080/sparrow-ocr")
     assert response.status_code == 200
     assert response.json() == {"message":"Sparrow OCR API"}
+
+def test_inference_marker(start_api_app_docker) -> None:
+    """
+    Test PDF conversion using marker
+    - Note: this may take 10s of seconds due to inference
+    """
+    response = requests.post("http://127.0.0.1:8080/marker/inference")
+    assert response.status_code == 200
+    assert response.json()['total_duration_in_second'] > 0
+    assert response.json()['result'][0]['filename'] == 'ms-note-one-page.pdf'
+
