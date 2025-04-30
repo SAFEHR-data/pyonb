d9c0ff4ae1429b9e436adba9fda78513eb46b757
Tom Roberts <tom.roberts@ucl.ac.uk>
2025-04-30 15:10:17 +0100

    QoL changes

--- tests/api/test_routers.py
+++ tests/api/test_routers.py
@@ -1,46 +1,49 @@
-"""Test functions in /src/api/app/routers."""
+"""
+Test functions in /src/api/app/routers.
 
+- Note: Tests require running Docker services.
+"""
 import pytest
 import requests
 
 
 @pytest.mark.usefixtures("start_api_app_docker")
-def test_router_marker(ocr_api_port: str) -> None:
-    """Test router for marker."""
-    response = requests.get(f"http://127.0.0.1:{ocr_api_port}/marker/health", timeout=5)
+def test_router_marker(ocr_forwarding_api_port: str) -> None:
+    """Test healthcheck for marker."""
+    response = requests.get(f"http://127.0.0.1:{ocr_forwarding_api_port}/marker/health", timeout=5)
     assert response.status_code == requests.codes.ok
     assert response.json() == {"service": "marker", "status": "healthy"}
 
 
 @pytest.mark.usefixtures("start_api_app_docker")
-def test_router_sparrow(ocr_api_port: str) -> None:
+def test_router_sparrow(ocr_forwarding_api_port: str) -> None:
     """
-    Test router for sparrow.
+    Test healthcheck for sparrow.
 
     - Note: uses sparrow devs' own API.
     """
-    response = requests.get(f"http://127.0.0.1:{ocr_api_port}/sparrow-ocr", timeout=5)
+    response = requests.get(f"http://127.0.0.1:{ocr_forwarding_api_port}/sparrow-ocr", timeout=5)
     assert response.status_code == requests.codes.ok
     assert response.json() == {"message": "Sparrow OCR API"}
 
 
 @pytest.mark.usefixtures("start_api_app_docker")
-def test_inference_marker(ocr_api_port: str) -> None:
+def test_inference_marker(ocr_forwarding_api_port: str) -> None:
     """
     Test PDF conversion using marker.
 
     - Note: this may take 10s of seconds due to inference
     """
-    response = requests.post(f"http://127.0.0.1:{ocr_api_port}/marker/inference", timeout=5)
+    response = requests.post(f"http://127.0.0.1:{ocr_forwarding_api_port}/marker/inference", timeout=5)
     assert response.status_code == requests.codes.ok
     assert response.json()["total_duration_in_second"] > 0
     assert response.json()["result"][0]["filename"] == "ms-note-one-page.pdf"
 
 
 @pytest.mark.usefixtures("start_api_app_docker")
-def test_inference_sparrow(ocr_api_port: str) -> None:
+def test_inference_sparrow(ocr_forwarding_api_port: str) -> None:
     """Test PDF conversion using sparrow."""
-    response = requests.post(f"http://127.0.0.1:{ocr_api_port}/sparrow-ocr/inference", timeout=5)
+    response = requests.post(f"http://127.0.0.1:{ocr_forwarding_api_port}/sparrow-ocr/inference", timeout=5)
     assert response.status_code == requests.codes.ok
     assert response.json()["total_duration_in_second"] > 0
     assert response.json()["result"][0]["filename"] == "ms-note-one-page.pdf"
