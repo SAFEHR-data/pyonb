5a7d90aa6a218c9f1f13f3072b51a09c73c0f719
Tom Roberts <tom.roberts@ucl.ac.uk>
2025-04-30 10:57:17 +0100

    Simplifies test_app tests

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -8,6 +8,7 @@ import time
 from collections.abc import Generator
 
 import pytest
+from _pytest.fixtures import SubRequest
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -30,12 +31,14 @@ class StartApiError(Exception):
         super().__init__("Cannot find main.py to start API.")
 
 
-def is_healthy(service: str) -> str | bool:
+@pytest.fixture(scope="module")
+def check_container_healthy(request: SubRequest) -> str | bool:
     """
-    Checks if Docker service is healthy based.
+    Checks if Docker service is healthy based on docker inspect health status.
 
     - Returns True if docker inspect <service> returns "healthy" status
     """
+    service = request.param
     try:
         result = subprocess.run(
             ["docker", "inspect", service],
@@ -57,25 +60,8 @@ def is_healthy(service: str) -> str | bool:
         return health_status == "healthy"
 
 
-def wait_for_service(docker_service_name: str, timeout: int = 180) -> bool:
-    """
-    Waits for Docker services to build and run.
-
-    - if services are not up within the timeout duration, TimeoutError raised
-    """
-    logger.info("Waiting for %s to become healthy...", docker_service_name)
-    start = time.time()
-    while time.time() - start < timeout:
-        if is_healthy(docker_service_name):
-            logger.info("%s is healthy!", docker_service_name)
-            return True
-        time.sleep(20)
-    e = f"{docker_service_name} did not become healthy in time."
-    raise TimeoutError(e)
-
-
 @pytest.fixture(scope="module")
-def ocr_api_port() -> str | None:
+def ocr_forwarding_api_port() -> str | None:
     """Returns OCR_FORWARDING_API_PORT."""
     if os.getenv("OCR_FORWARDING_API_PORT"):
         return os.getenv("OCR_FORWARDING_API_PORT")
@@ -84,7 +70,7 @@ def ocr_api_port() -> str | None:
 
 
 @pytest.fixture(scope="module")
-def start_api_app() -> Generator:
+def local_start_api_app() -> Generator:
     """
     Starts the forwarding API on the local host machine.
 
@@ -110,36 +96,6 @@ def start_api_app() -> Generator:
     time.sleep(2)
 
     yield  # Yield control to tests
-
-    proc.terminate()
-    proc.wait()
-
-
-@pytest.fixture(scope="module")
-def start_api_app_docker() -> Generator:
-    """Starts forwarding API and OCR tool APIs with Docker."""
-    proc = subprocess.Popen(
-        [
-            "docker",
-            "compose",
-            "--profile",
-            "marker",
-            "--profile",
-            "sparrow",
-            "up",
-            "-d",
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    time.sleep(2)
-
-    wait_for_service("pyonb-ocr-forwarding-api-1")  # TODO(tom): preferable not to hardcode service strings
-    wait_for_service("pyonb-marker-1")
-    wait_for_service("pyonb-sparrow-1")
-
-    yield
 
     proc.terminate()
     proc.wait()
--- tests/api/test_app.py
+++ tests/api/test_app.py
@@ -4,17 +4,23 @@ import pytest
 import requests
 
 
-@pytest.mark.usefixtures("start_api_app")
-def test_start_api_and_healthy(ocr_api_port: str) -> None:
+@pytest.mark.usefixtures("local_start_api_app")
+def test_local_start_api_and_healthy(ocr_forwarding_api_port: str) -> None:
     """Test API app comes up and healthcheck returns 200."""
-    response = requests.get(f"http://127.0.0.1:{ocr_api_port}/", timeout=5)
+    response = requests.get(f"http://127.0.0.1:{ocr_forwarding_api_port}/", timeout=5)
     assert response.status_code == requests.codes.ok
     assert response.json() == {"service": "pyonb-ocr-api", "status": "healthy"}
 
 
-@pytest.mark.usefixtures("start_api_app_docker")
-def test_start_api_and_healthy_docker(ocr_api_port: str) -> None:
-    """Test API app comes up and healthcheck returns 200."""
-    response = requests.get(f"http://127.0.0.1:{ocr_api_port}/", timeout=5)
-    assert response.status_code == requests.codes.ok
-    assert response.json() == {"service": "pyonb-ocr-api", "status": "healthy"}
+@pytest.mark.parametrize(
+    "check_container_healthy",
+    [
+        "pyonb-ocr-forwarding-api-1",
+        "pyonb-marker-1",
+        "pyonb-sparrow-1",
+    ],
+    indirect=True,
+)
+def test_check_services(check_container_healthy: bool) -> None:
+    """Test OCR API Docker services are up."""
+    assert check_container_healthy, "Docker services not running. Required for full API testing."
