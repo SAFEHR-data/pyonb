f92e2c2aa90e3d1d132ea77324f09940fdbdd65c
Tom Roberts <tom.roberts@ucl.ac.uk>
2025-05-27 18:10:29 +0100

    Add check_data_folder() inline
    Temporary solution as using util.check_data_folder causing import issues inside Docker 😿

--- src/api/app/routers/marker.py
+++ src/api/app/routers/marker.py
@@ -12,8 +12,6 @@ from dotenv import load_dotenv
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 
-from api.app.util import check_data_folder
-
 load_dotenv()
 
 if os.getenv("MARKER_API_PORT"):
@@ -26,6 +24,30 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 router = APIRouter()
+
+
+def check_data_folder() -> Path | str:
+    """Check if Docker or local deployment and adjust DATA_FOLDER accordingly."""
+    # Detect if in Docker container
+    is_docker = Path("/.dockerenv").exists()
+
+    logger.info("HOST_DATA_FOLDER: %s", str(os.environ.get("HOST_DATA_FOLDER")))
+
+    if is_docker:
+        logger.info("Detected running inside Docker container.")
+        DATA_FOLDER = str(os.environ.get("CONTAINER_DATA_FOLDER"))
+    elif not is_docker:
+        logger.info("Detected running on host machine.")
+        DATA_FOLDER = str(os.environ.get("HOST_DATA_FOLDER"))
+
+    if Path(DATA_FOLDER).exists():
+        logger.info("DATA_FOLDER: %s", DATA_FOLDER)
+    else:
+        e = f"{Path(DATA_FOLDER)!s} not found or does not exist."
+        logger.exception(NotADirectoryError(e))
+        raise NotADirectoryError(e)
+
+    return DATA_FOLDER
 
 
 @router.get("/marker/health")
