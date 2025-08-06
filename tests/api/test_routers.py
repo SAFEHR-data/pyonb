7b5a682cb620ac1373b4bf4c146f0c90d8331602
Paul Smith <paul.j.smith@ucl.ac.uk>
2025-08-06 11:24:38 +0100

    Add a test for the docling healthcheck

--- tests/api/test_routers.py
+++ tests/api/test_routers.py
@@ -28,10 +28,17 @@ def test_router_sparrow(ocr_forwarding_api_port: str) -> None:
 
 
 def test_router_paddleocr(ocr_forwarding_api_port: str) -> None:
-    """Test healthcheck for marker."""
+    """Test healthcheck for paddleocr."""
     response = requests.get(f"http://127.0.0.1:{ocr_forwarding_api_port}/paddleocr/health", timeout=5)
     assert response.status_code == requests.codes.ok
     assert response.json() == {"service": "paddleocr", "status": "healthy"}
+
+
+def test_router_docling(ocr_forwarding_api_port: str) -> None:
+    """Test healthcheck for docling."""
+    response = requests.get(f"http://127.0.0.1:{ocr_forwarding_api_port}/docling/health", timeout=5)
+    assert response.status_code == requests.codes.ok
+    assert response.json() == {"service": "docling", "status": "healthy"}
 
 
 def test_inference_single_file_upload_marker(ocr_forwarding_api_port: str, single_pdf_filepath: Path) -> None:
